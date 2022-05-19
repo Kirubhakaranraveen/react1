8fe9f7baa263753f1824669b7254805e82fbb935
Kirubhakaranpraveen <98150134+Kirubhakaranraveen@users.noreply.github.com>
2022-05-19 19:16:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import App from "./App.js";
+
+function App() {
+  return (
+    <div className="App">
+     <previous/>
+     <Functional/>
+     <Class/>
+    </div>
+  );
+}
+
+export default App;
